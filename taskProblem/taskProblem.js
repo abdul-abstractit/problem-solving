65eaf05f7971a5dd597fdf961200747537b8f63f
abdul-abstractit <ars@abstractit.in>
2022-11-14 18:49:31 +0530

    refactored

--- taskProblem/taskProblem.js
+++ taskProblem/taskProblem.js
@@ -1,6 +1,6 @@
 const { map, reduce } = require("@laufire/utils/collection");
+const { pretty, peek } = require("@laufire/utils/debug");
 const task = require("./data");
-
 
 const calcSubTasksCost = (tasks) => reduce(tasks, (acc, {cost}) => acc + cost, 0);
 
@@ -11,8 +11,12 @@ const calculateCost = (task) => {
   return ({
     ...task,
     cost: cost + calcSubTasksCost(calculatedSubTasks),
-    tasks: calculatedSubTasks
+    tasks: calculatedSubTasks,
   })
 }
 
-console.log(calculateCost(task));
+const main = (task) => {
+  peek(pretty(calculateCost(task),2));
+};
+
+main(task);
