82319a41db171ad29501261632dbcf2a5bfa96df
abdul-abstractit <ars@abstractit.in>
2022-11-17 13:45:05 +0530

    refactored.

--- replProblem.js
+++ replProblem.js
@@ -1,24 +1,24 @@
 const { reduce } = require("@laufire/utils/collection");
 
-const operation = (n1,n2,o) => {
+const operation = (n1, n2, o) => {
   let result
-  if (o === '+') result = n1+n2
-  if (o === '-') result = n1-n2
-  if (o === '*') result = n1*n2
-  if (o === '/') result = n1/n2
+  if (o === '+') result = n1 + n2
+  if (o === '-') result = n1 - n2
+  if (o === '*') result = n1 * n2
+  if (o === '/') result = n1 / n2
   return result
-} 
+};
 
-const result = reduce(
-  ["2","+","5","/","3","+","2","+","5"],
-  (acc, num, i, arr)=>{
-    const result = (num==='+'||num==='-'||num==='*'||num==='/')
-    ? operation(parseInt(arr[i-1]),parseInt(arr[i+1]),num) 
+const calc = (inpArray) => reduce(inpArray, (acc, num, i, arr) => {
+  const result = (num === '+' || num === '-' || num === '*' || num === '/')
+    ? operation(parseInt(arr[i - 1]), parseInt(arr[i + 1]), num)
     : acc;
-   
-    arr[i+1]= operation(parseInt(arr[i-1]),parseInt(arr[i+1]),num) || arr[i+1]
-    return result
-  },
-  0)
+
+  arr[i + 1] = operation(parseInt(arr[i - 1]), parseInt(arr[i + 1]), num) || arr[i + 1]
+  return result
+}, 0)
+
+
+const result = calc(["2", "+", "5", "/", "3", "+", "2", "+", "5"]);
 
 console.log(result);
