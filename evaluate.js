e93bb6810ef7766e526dcf6918929d95b8be8ff7
abdul-abstractit <ars@abstractit.in>
2022-11-19 12:39:55 +0530

    refactored again.

--- evaluate.js
+++ evaluate.js
@@ -8,26 +8,24 @@ const operations = {
   '/': (num1, num2) => num1 / num2,
 }
 
-const calculate = ({stack,input}) => {
+const calculate = ({ stack, input }) => {
   const second = stack.pop();
   const first = stack.pop();
-  const result = operations[input](first,second)
+  const result = operations[input](first, second)
 
-  return {stack: [...stack,result],result}
+  return { stack: [...stack, result] }
 }
+const pushToStack = ({stack,input}) => ({ stack: [...stack, input] })
 
-const evaluate = (inputs) => {
-  const standardInputs = infixToPostfix(inputs)
-  return reduce(standardInputs, ({stack,result}, input) => {
-    const isOperator = !!operations[input];
-    return isOperator ? calculate({stack,result,input}) : { result, stack: [...stack, input] }
-  }, { stack: [], result: 0 }).result
-}
+const evaluatePostfix = ({ stack }, input) => !!operations[input] 
+  ? calculate({ stack, input }) 
+  : pushToStack({ stack, input })
+
+const evaluate = (inputs) => reduce(
+  infixToPostfix(inputs), 
+  evaluatePostfix, 
+  { stack: [] }
+).stack[0]
+
 
 module.exports = evaluate;
-
-/*
-Evaluation rule of a Postfix Expression states:
-While reading the expression from left to right, push the element in the stack if it is an operand. Pop the two operands from the stack, if the element is an operator and then evaluate it. Push back the result of the evaluation. Repeat it till the end of the expression.
-*/
-
